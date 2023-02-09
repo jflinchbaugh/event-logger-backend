09f07d55570cf0202048237dd71dce243097ca5f
John Flinchbaugh <john@hjsoft.com>
2023-02-08 23:35:14 -0500

    more deps and routing

--- src/event_logger_backend/core.clj
+++ src/event_logger_backend/core.clj
@@ -5,28 +5,42 @@
 
 (defn handler [req]
   {:status  200
-   :headers {"Content-Type" "text/html"}
+   :headers {"Content-Type" "text/plain"}
    :body    (str "hello HTTP! " req)})
+
+(defn login-form [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    (str "hello HTTP! " req)})
+
+(defn login-action [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    (str "hello HTTP! " req)})
+
+(defn not-found [& _]
+  {:status 404
+   :headers {"Content-Type" "text/plain"}
+   :body "Not Found"})
 
 (def app
   (ring/ring-handler
     (ring/router
-      [["/api" ["/upload" {:get handler}]]
-       ])))
+      [["/api"
+        ["/upload" {:get handler}]
+        ["/auth" {:get login-form
+                  :post login-action}]]
+       ])
+    not-found))
 
 (defonce server (atom nil))
 
 (defn stop-server! []
   (when-not (nil? @server)
-    ;; graceful shutdown: wait 100ms for existing requests to be finished
-    ;; :timeout is optional, when no timeout, stop immediately
     (@server :timeout 100)
     (reset! server nil)))
 
 (defn start-server! []
-  ;; The #' is useful when you want to hot-reload code
-  ;; You may want to take a look: https://github.com/clojure/tools.namespace
-  ;; and https://http-kit.github.io/migration.html#reload
   (reset! server (hks/run-server #'app {:port 8080})))
 
 (defn -main [& args]

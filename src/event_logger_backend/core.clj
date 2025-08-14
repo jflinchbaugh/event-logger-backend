dc220bcb7377d4cf9708ab7792afe2a3328deb5e
John Flinchbaugh <john@hjsoft.com>
2025-08-14 10:07:10 -0400

    show the full resource name when it's created

--- src/event_logger_backend/core.clj
+++ src/event_logger_backend/core.clj
@@ -97,7 +97,8 @@
   [req]
   (let [id (get-in req [:params :id])
         login (get-in req [:params :login])
-        password (get-in req [:params :password])]
+        password (get-in req [:params :password])
+        resource (format "%s/api/logger/%s" base-url id)]
     (if (get @storage id)
       (api-response 200 (format "'%s' already exists" id))
       (do
